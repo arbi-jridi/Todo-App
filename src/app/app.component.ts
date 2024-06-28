a68254291bbfbb77c8646c10f4c51bf24ad28fe0
Arbi Jridi <110457849+arbi-jridi@users.noreply.github.com>
2024-06-28 15:00:44 +0100

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+interface Task {
+  text: string;
+  done: boolean;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'toDoList';
+  @Input() task!: { text: string; done: boolean };
+
+  tasks: Task[] = [];
+  newTask: string = '';
+
+  onAdd() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push({ text: this.newTask, done: false });
+      this.newTask = '';
+    } else {
+      alert('Please enter a task !');
+    }
+  }
+
+  toggleTask(index: number) {
+    this.tasks[index].done = !this.tasks[index].done;
+    console.log(this.tasks[index].done);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
